9f6a076eb92166ed3f0d885ea3bc1a9ccf5635a8
Christian <christianpavilonis@outlook.com>
2024-11-30 07:51:26 -0600

    Spaces footer away from content darkens the border color

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
 
 function Footer() {
   return (
-    <footer className="border-t bg-background ">
+    <footer className="border-t border-surface1 bg-background mt-24">
       <div className="container max-w-[1280px] mx-auto flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
