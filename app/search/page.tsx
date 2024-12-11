7b2e1f03468a2788d244b238ca55016b7626bee3
Conner Ow <conner@connerow.dev>
2024-12-11 09:02:45 -0600

    undo force push

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,8 @@
-import { Suspense } from "react"
 import { getAllArticles } from "../lib/articles"
 import SearchContent from "./content"
 
 export default async function Search() {
   const articles = getAllArticles()
 
-  return (
-    <Suspense>
-      <SearchContent articles={articles} />
-    </Suspense>
-  )
+  return <SearchContent articles={articles} />
 }
